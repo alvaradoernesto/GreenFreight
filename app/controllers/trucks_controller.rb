df861ac03192f5a994ab2a3d90c9ec80252bc8ac
Mauricio Mas <mauriciomastonon@gmail.com>
2020-08-11 21:55:48 -0300

    Navbar and truck new fix

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,23 +1,24 @@
 class TrucksController < ApplicationController
   def new
     @truck = Truck.new
+    @truck_load_category = TruckLoadCategory.new
   end
 
   def create
     @truck = Truck.new(truck_params)
     @truck.user = current_user
-    @truck_load_category = TruckLoadCategory.new(
-      load_category: LoadCategory.find_by(name: params[:truck][:load_category_ids].reject(&:blank?)),
-      truck: @truck)
-    if @truck_load_category.save
       if @truck.save
+        truck_load_params
+        params[:truck][:truck_load_categories_attributes]["0"][:id].reject(&:blank?).each do |load|
+          @load_cat = TruckLoadCategory.new(
+             load_category_id: load,
+             truck_id: @truck.id)
+          @load_cat.save!
+        end
         redirect_to truck_path(@truck)
       else
         render :new
       end
-    else
-      render :new
-    end
   end
 
   def show
@@ -30,6 +31,10 @@ class TrucksController < ApplicationController
     params.require(:truck)
           .permit(:capacity, :truck_name,
           :driver_name, :driver_license,
-          :truck_license, :price_per_km, :truck_category_id, [:load_category_id])
+          :truck_license, :price_per_km, :truck_category_id, truck_load_categories: [:id])
+  end
+
+  def truck_load_params
+    params[:truck]["truck_load_categories_attributes"].permit!
   end
 end
--- app/models/truck_load_category.rb
+++ app/models/truck_load_category.rb
@@ -3,5 +3,4 @@ class TruckLoadCategory < ApplicationRecord
   belongs_to :load_category
 
   validates :truck, uniqueness: { scope: :load_category }
-
 end
