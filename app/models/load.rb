b8319957ba9172844a1e360dc7bdbcc953f8382e
Nadia Salmen <nadiasalmen@gmail.com>
2020-08-15 12:37:38 -0300

    add datepicker to search form - fix search form

--- app/models/load.rb
+++ app/models/load.rb
@@ -1,8 +1,10 @@
 class Load < ApplicationRecord
+  STATUSES = ["Nueva", "Asignada", "En tránsito", "Entregada", "Cancelada"]
+
   belongs_to :user
   belongs_to :freight, optional: true
   has_many :load_category
   has_many :special_requirement
   has_many :addresses
-  validates :status, inclusion: {in: ["Nueva", "Asignada", "En tránsito", "Entregada", "Cancelada"]}
+  validates :status, inclusion: {in: STATUSES}
 end
