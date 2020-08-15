8b8009fbc7e41251383cb60f3609dabf802e2ec4
Ernesto Alvarado <alvaradoernesto@hotmail.com>
2020-08-15 15:17:13 -0300

    Falta corregir los markers

--- app/models/load.rb
+++ app/models/load.rb
@@ -3,6 +3,9 @@ class Load < ApplicationRecord
   belongs_to :freight, optional: true
   has_many :load_category
   has_many :special_requirement
-  has_many :addresses
+  has_one :start_point
+  has_one :end_point
+  accepts_nested_attributes_for :start_point
+  accepts_nested_attributes_for :end_point
   validates :status, inclusion: {in: ["Nueva", "Asignada", "En tránsito", "Entregada", "Cancelada"]}
 end
---
+++ app/models/start_point.rb
@@ -0,0 +1,5 @@
+class StartPoint < ApplicationRecord
+  belongs_to :load
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+end
