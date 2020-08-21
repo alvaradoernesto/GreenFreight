eee73f3a0f3ee06b222d9bd0500c196ab2403629
Ernesto Alvarado <alvaradoernesto@hotmail.com>
2020-08-21 20:25:43 -0300

    arreglo de load show y new

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -40,7 +40,8 @@ class LoadsController < ApplicationController
   def create
     @load = Load.new(load_params)
     @load.user = current_user
-    if @load.save
+    @load.status = "Nueva"
+    if @load.save!
       redirect_to load_path(@load)
     else
       render :new
@@ -67,7 +68,7 @@ class LoadsController < ApplicationController
   private
 
   def load_params
-    params.require(:load).permit(:user, :start_date, :end_date, :hour_range,
-                                 :weight, :volume, :status, :price, start_point_attributes: [:location, :latitude, :longitude], end_point_attributes: [:location, :latitude, :longitude])
+    params.require(:load).permit(:user, :start_date, :end_date, :hour_range, :load_category_id, :special_requirement_id,
+                                 :weight, :volume, start_point_attributes: [:location, :latitude, :longitude], end_point_attributes: [:location, :latitude, :longitude])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # post 'cotizacion', to: 'pages#cotizacion'
   get 'cotizacion', to: 'pages#cotizacion'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :loads
+  resources :loads, only: [:new, :create, :new, :index, :show]
   resources :trucks, only: [:new, :create]
   resources :freights, only: [:new, :create, :index, :show]
   resources :pickings, only: [:new, :create, :index]
