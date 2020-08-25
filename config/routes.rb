09a621b912ce1a4d9973e8b094d9a0e249b5de47
Mauricio Mas <mauriciomastonon@gmail.com>
2020-08-25 19:31:06 -0300

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,12 @@ Rails.application.routes.draw do
   get 'cotizacion', to: 'pages#cotizacion'
   get 'myloads', to: 'loads#myloads'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :loads, only: [:new, :create, :new, :index, :show]
+  resources :loads, only: [:new, :create, :new, :index, :show] do
+    member do
+        patch :add_load
+        put :add_load
+    end
+  end
   resources :trucks, only: [:new, :create]
   resources :freights, only: [:new, :create, :index, :show]
   resources :pickings, only: [:new, :create, :index]
